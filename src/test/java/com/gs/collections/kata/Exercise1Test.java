ad17e82d4424cda24e518787643886eee1562b1a
Zax Foster <Zax.Foster@gmail.com>
2013-01-27 02:03:21 -0600

    exercise1

--- src/test/java/com/gs/collections/kata/Exercise1Test.java
+++ src/test/java/com/gs/collections/kata/Exercise1Test.java
@@ -17,37 +17,35 @@
 package com.gs.collections.kata;
 
 import com.gs.collections.api.block.function.Function;
+import com.gs.collections.api.block.predicate.Predicate;
 import com.gs.collections.api.list.MutableList;
 import com.gs.collections.impl.list.mutable.FastList;
 import com.gs.collections.impl.test.Verify;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class Exercise1Test extends CompanyDomainForKata
-{
-    @Test
-    public void getCustomerNames()
-    {
-        Function<Customer, String> nameFunction = new Function<Customer, String>()
-        {
-            @Override
-            public String valueOf(Customer customer)
-            {
-                return customer.getName();
-            }
-        };
+public class Exercise1Test extends CompanyDomainForKata {
+	@Test
+	public void getCustomerNames() {
+		Function<Customer, String> nameFunction = new Function<Customer, String>() {
+			@Override
+			public String valueOf(Customer customer) {
+				return customer.getName();
+			}
+		};
 
-        /**
-         * Get the name of each of the company's customers.
-         */
-        MutableList<Customer> customers = this.company.getCustomers();
-        MutableList<String> customerNames = null;
+		/**
+		 * Get the name of each of the company's customers.
+		 */
+		MutableList<Customer> customers = this.company.getCustomers();
+		MutableList<String> customerNames = customers.collect(nameFunction);
 
-        MutableList<String> expectedNames = FastList.newListWith("Fred", "Mary", "Bill");
-        Assert.assertEquals(expectedNames, customerNames);
-    }
+		MutableList<String> expectedNames = FastList.newListWith("Fred", "Mary",
+				"Bill");
+		Assert.assertEquals(expectedNames, customerNames);
+	}
 
-    @Test
+	@Test
     public void getCustomerCities()
     {
         /**
@@ -56,20 +54,34 @@ public class Exercise1Test extends CompanyDomainForKata
          * ctrl+i in IntelliJ. Eclipse's ctrl+1 is auto-fix and works to implement interfaces.
          */
         MutableList<Customer> customers = this.company.getCustomers();
-        MutableList<String> customerCities = null;
+        MutableList<String> customerCities = customers.collect(
+        		new Function<Customer, String>() {
+        			public String valueOf(Customer c) 
+        			{
+        				return c.getCity();
+        			}
+        		}
+        	);
 
         MutableList<String> expectedCities = FastList.newListWith("London", "Liphook", "London");
         Assert.assertEquals(expectedCities, customerCities);
     }
 
-    @Test
-    public void getLondonCustomers()
-    {
-        /**
-         * Which customers come from London? Get a collection of those which do. Use an anonymous inner class.
-         */
-        MutableList<Customer> customers = this.company.getCustomers();
-        MutableList<Customer> customersFromLondon = null;
-        Verify.assertSize("Should be 2 London customers", 2, customersFromLondon);
-    }
+	@Test
+	public void getLondonCustomers() {
+		/**
+		 * Which customers come from London? Get a collection of those which do. Use
+		 * an anonymous inner class.
+		 */
+		MutableList<Customer> customers = this.company.getCustomers();
+		MutableList<Customer> customersFromLondon = customers.select(
+				new Predicate<Customer>() 
+				{
+					public boolean accept(Customer c) 
+					{
+						return c.getCity().equals("London");
+					}
+				});
+		Verify.assertSize("Should be 2 London customers", 2, customersFromLondon);
+	}
 }
